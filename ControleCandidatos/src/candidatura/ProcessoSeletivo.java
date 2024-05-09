25f3af7b25781d4207b5bef5a0ee411c37f7a94f
Nosliejnav <vanjeilsonm10@gmail.com>
2024-05-09 19:17:34 -0300

    adicionando lista e for

--- ControleCandidatos/src/candidatura/ProcessoSeletivo.java
+++ ControleCandidatos/src/candidatura/ProcessoSeletivo.java
@@ -4,10 +4,17 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 		public static void main(String[] args) {
+			imprimirSelecionados();
 		}
 		
 		static void imprimirSelecionados() {
 			String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+			
+			System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+			
+			for(int indice=0; indice < candidatos.length;indice++) {
+				System.out.println("O candidato de n* " + (indice+1) + " é " + candidatos[indice]);
+			}
 		}
 		
 		
